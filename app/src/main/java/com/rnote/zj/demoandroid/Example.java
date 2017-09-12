bafa7a4206f25fc5a64f6be73f4224e69daf7bc8
zhangsan <1072187222@qq.com>
2017-09-12 17:54:39 +0800

    test

--- app/src/main/java/com/rnote/zj/demoandroid/Example.java
+++ app/src/main/java/com/rnote/zj/demoandroid/Example.java
@@ -6,16 +6,26 @@ package com.rnote.zj.demoandroid;
 
 public class Example {
     //region 私有字段
+    private int id;
     //endregion
 
     //region 属性访问器
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     //endregion
 
     //region  私有方法
     //endregion
 
     //region 对外接口方法
-
+    //endregion
     //region 重载方法
     //endregion
 }
