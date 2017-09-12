5d48aa7f7897818520ee7863d46ccf06f717bf05
zhangsan <1072187222@qq.com>
2017-09-12 17:52:47 +0800

    test

---
+++ app/src/main/java/com/rnote/zj/demoandroid/Example.java
@@ -0,0 +1,21 @@
+package com.rnote.zj.demoandroid;
+
+/**
+ * Created by admin on 2017/9/12.
+ */
+
+public class Example {
+    //region 私有字段
+    //endregion
+
+    //region 属性访问器
+    //endregion
+
+    //region  私有方法
+    //endregion
+
+    //region 对外接口方法
+
+    //region 重载方法
+    //endregion
+}
--- app/src/main/java/com/rnote/zj/demoandroid/activity/MainActivity.java
+++ app/src/main/java/com/rnote/zj/demoandroid/activity/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import com.rnote.zj.demoandroid.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
